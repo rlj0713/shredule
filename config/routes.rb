07a4ff5a7843ee87ed56fca7558c0b00eeaf7b48
Reid <rlj0713@gmail.com>
2021-12-07 20:45:29 -0500

    Added basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+
+  get '/auth/:provider/callback' => 'google#create'
+
+  get "signup" => 'users#new'
+  get "login" => 'sessions#new'
+
+  post '/sessions' => 'sessions#create'
+  delete '/sessions' => 'sessions#destroy'
+
+  resources :sessions
+  resources :users
 end
