8a0736c04dfb4d4612ee2558c3ec4dff856f09d4
Reid <rlj0713@gmail.com>
2022-02-15 17:23:23 -0500

    removed admin class, changed to attribute of user

--- db/migrate/20220212210115_create_admin_list.rb
+++
@@ -1,9 +0,0 @@
-class CreateAdminList < ActiveRecord::Migration[6.1]
-  def change
-    create_table :admin_lists do |t|
-      t.integer "user_id"
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20220212210313_create_admins.rb
+++
@@ -1,9 +0,0 @@
-class CreateAdmins < ActiveRecord::Migration[6.1]
-  def change
-    create_table :admins do |t|
-      t.integer :user_id
-
-      t.timestamps
-    end
-  end
-end
