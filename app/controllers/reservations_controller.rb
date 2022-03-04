eda7d723f256e7046d33506c1c91bd010224c70e
Reid <rlj0713@gmail.com>
2022-03-03 23:05:11 -0500

    added create reservation function

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,15 @@
 class ReservationsController < ApplicationController
+    def create
+        if @reservation.valid?
+            @reservation.save
+            redirect_to reservations_path(@reservation)
+        end
+    end
+    
+    def new
+        @reservation = Reservation.new
+    end
+    
     def index
         @reservations = Reservation.all
         @customers = Customer.all
