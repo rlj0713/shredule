1594ed8e15683d436abbb30a2a5d42ff4c2d23d0
Reid <rlj0713@gmail.com>
2022-03-03 23:06:38 -0500

    removed admin class & relationship from user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@
 class User < ActiveRecord::Base
     has_secure_password
 
-    belongs_to :admin, optional: true
-
     validates :email, presence: true, uniqueness: true
     validates :password_digest, presence: true
 end
